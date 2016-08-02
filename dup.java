f500cc5efa4f434db9888757427b1244ace786ea
DeepikaSugumar <s.deepisugu@gmail.com>
2016-08-02 05:22:44 -0700

    ghgkhgkhk
    jfhskjlkh

---
+++ dup.java
@@ -0,0 +1,21 @@
+public class test {
+
+	public static void main(String[] args) {
+		
+		 String s = "deepika";
+	        String s2 = "";
+	        for (int i = 0; i < s.length(); i++) {
+	            Boolean found = false;
+	            for (int j = 0; j < s2.length(); j++) {
+	                if (s.charAt(i) == s2.charAt(j)) {
+	                    found = true;
+	                    break; 
+	                }
+	            }
+	            if (found == false) {
+	                s2 = s2.concat(String.valueOf(s.charAt(i)));
+	            }
+	        }
+	        System.out.println(s2);
+	    }
+	}
